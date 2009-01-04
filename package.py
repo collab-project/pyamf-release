aa512bb52deb4b1fa0e5c0c1c5b6f0f5fcf4b8c4
Thijs Triemstra <info@collab.nl>
2009-01-04 03:19:48 +0000

    Update release packaging script:  - include cPyAMF docs by building the C-extension  - use setup.cfg file for epydoc  - display md5 checksums

--- package.py
+++ package.py
@@ -1,19 +1,26 @@
-# Copyright (c) 2007-2008 The PyAMF Project.
+#!/usr/bin/python
+
+# Copyright (c) 2007-2009 The PyAMF Project.
 # See LICENSE for details.
 
 """
 A basic release builder.
 
 Example usage:
-  python package.py --version=0.3 --name=PyAMF --package=pyamf --baseurl=http://svn.pyamf.org/pyamf/tags --branch=release-0.3
-
-@author: U{Nick Joyce<mailto:nick@boxdesign.co.uk>}
+  python package.py --version=0.3 --name=PyAMF --baseurl=http://svn.pyamf.org/pyamf/tags --branch=release-0.3
+  
+For more help try:
+  python package.py --help
 """
 
 import os, os.path, sys, shutil
 import subprocess, tempfile
+import hashlib
 
 def export_svn(url, options):
+    print
+    print 'Checking out: %s\n' % url
+    
     args = ['svn', 'export']
 
     args.append(url)
@@ -42,16 +49,34 @@ def export_repo(options):
         raise ValueError, "Unknown repo type %s" % options.repo
 
     if retcode != 0:
-        raise RuntimeError, "problem exporting repo"
-
+        raise RuntimeError, "Problem exporting repository"
+    
     return export_dir
+        
+def build_ext(src_dir):
+    cwd = os.getcwd()
+    os.chdir(src_dir)
+    
+    args = ["python", "setup.py", "build"]
 
+    cmd = subprocess.Popen(args)
+    retcode = cmd.wait()
+    
+    if retcode != 0:
+        raise RuntimeError, "Error building extension"
+    
+    os.chdir(cwd)
+    
 def build_api_docs(src_dir, options):
+    # create .so files for epydoc
+    build_ext(export_dir)
+    
     api_dir = tempfile.mkdtemp()
-    args = ["epydoc", "--output=%s" % api_dir, "--simple-term",
-        "--show-imports", "--graph=umlclasstree", "--dotpath=/usr/bin/dot",
-        "--name=%s API Documentation" % options.name, "--exclude=tests*",
-        os.path.join(src_dir, options.package)]
+    
+    args = ["epydoc", "--config=" +
+        os.path.join(src_dir, "setup.cfg"),
+        "--graph=umlclasstree", "--dotpath=" + options.dotpath,
+        "-v"]
 
     cmd = subprocess.Popen(args)
     retcode = cmd.wait()
@@ -59,6 +84,14 @@ def build_api_docs(src_dir, options):
     if retcode != 0:
         raise RuntimeError, "Error building API docs"
 
+    # the --output override for epydoc doesn't work
+    # so copy the files and get rid of the old default
+    # output destination folder
+    temp_doc = os.path.join(os.getcwd(), "docs")
+    api_dir = os.path.join(api_dir, "docs")
+    shutil.copytree(temp_doc, api_dir)
+    shutil.rmtree(temp_doc)
+    
     return api_dir
 
 def parse_args():
@@ -73,11 +106,13 @@ def parse_args():
     parser.add_option('--branch', dest='branch', default='trunk')
     parser.add_option('--version', dest='version')
     parser.add_option('--name', dest='name', help='The name of the project')
-    parser.add_option('--package', dest='package', help='The name of the package')
-    parser.add_option('--username', dest='username', default=None, help='Username to access the repo')
-    parser.add_option('--password', dest='password', default=None, help='Password to access the repo')
+    parser.add_option('--username', dest='username', default=None,
+                                    help='Username to access the repo')
+    parser.add_option('--password', dest='password', default=None,
+                                    help='Password to access the repo')
     parser.add_option('--repo', dest='repo', default='svn')
-
+    parser.add_option('--dotpath', dest='dotpath', default='/usr/bin/dot')
+    
     return parser.parse_args()
 
 def build_zip(build_dir, options):
@@ -108,7 +143,7 @@ def build_tar_gz(build_dir, options):
     retcode = cmd.wait()
 
     if retcode != 0:
-        raise RuntimeError, "Error creating zip"
+        raise RuntimeError, "Error creating tar.gz"
 
     os.chdir(cwd)
 
@@ -125,7 +160,7 @@ def build_tar_bz2(build_dir, options):
     retcode = cmd.wait()
 
     if retcode != 0:
-        raise RuntimeError, "Error creating zip"
+        raise RuntimeError, "Error creating tar.bz2"
 
     os.chdir(cwd)
 
@@ -134,7 +169,7 @@ def build_tar_bz2(build_dir, options):
 def clean_package_dir(package_dir):
     for files in os.walk(package_dir):
         for f in files[2]:
-            if f.endswith('.pyc'):
+            if f.endswith('.pyc') or f.endswith('.so'):
                 os.unlink(os.path.join(files[0], f))
 
 def package_project(src_dir, api_dir, options):
@@ -143,14 +178,35 @@ def package_project(src_dir, api_dir, options):
     """
     build_dir = tempfile.mkdtemp()
     package_dir = os.path.join(build_dir, '%s-%s' % (options.name, options.version))
+    shutil.rmtree(os.path.join(src_dir, 'build'))
     shutil.copytree(src_dir, package_dir)
     shutil.copytree(api_dir, os.path.join(package_dir, 'doc', 'api'))
-
+    
     clean_package_dir(package_dir)
 
     return build_dir, build_zip(build_dir, options), \
         build_tar_gz(build_dir, options), build_tar_bz2(build_dir, options)
 
+def md5(fileName, excludeLine="", includeLine=""):
+    """
+    Compute md5 hash of the specified file.
+    """
+    m = hashlib.md5()
+    try:
+        fd = open(fileName, "rb")
+    except IOError:
+        print "Unable to open the file:", filename
+        return
+    content = fd.readlines()
+    fd.close()
+    for eachLine in content:
+        if excludeLine and eachLine.startswith(excludeLine):
+            continue
+        m.update(eachLine)
+    m.update(includeLine)
+    
+    return m.hexdigest()
+    
 if __name__ == '__main__':
     options, args = parse_args()
 
@@ -158,22 +214,26 @@ if __name__ == '__main__':
         raise ValueError, "baseurl arg is required"
     if options.name is None:
         raise ValueError, "project name is required"
-    if options.package is None:
-        raise ValueError, "package name is required"
     if options.version is None:
         raise ValueError, "version is required"
 
+    cwd = os.getcwd()
     export_dir = export_repo(options)
     sys.path.insert(0, export_dir)
     api_dir = build_api_docs(export_dir, options)
-
+    
     build_dir = package_project(export_dir, api_dir, options)
     archives = build_dir[1:]
     build_dir = build_dir[0]
 
+    print '=' * 80
     for archive in archives:
-        shutil.copy(archive, os.getcwd())
-
+        print '%s  ./%s' % (md5(archive), os.path.basename(archive))
+        shutil.copy(os.path.join(build_dir, archive), cwd)
+    print '=' * 80
+    
     shutil.rmtree(export_dir)
-    shutil.rmtree(api_dir)
     shutil.rmtree(build_dir)
+    shutil.rmtree(os.path.dirname(api_dir))
+    
+    print '\n%s-%s ready.\n' % (options.name, options.version) 
